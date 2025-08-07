d9a7cd09e3fe1114c3b26333cd48b0345d82292f
Charlotte Kostelic <charlottekostelic@gmail.com>
2025-08-07 16:28:02 -0400

    removed `scope` and `token_type` attrs, changed `is_expired` to property

--- bookops_overdrive/authorize.py
+++ bookops_overdrive/authorize.py
@@ -1,21 +1,30 @@
 """Provides client authentication for Overdrive APIs"""
 
+from __future__ import annotations
+
 import datetime
+import sys
 
 import requests
 
 from . import __title__, __version__
+from .errors import BookopsOverdriveError
 
 
 class OverdriveAccessToken:
     """
-    Requests an access token from Overdrive oauth2 server.
+    Requests an access token from Overdrive OAuth2 server.
 
     Args:
-        key: API key as a string
-        secret: API secret as a string
-        resource_url: url for oauth server
-        agent: `User-agent` parameter to be passed in request header
+        key:
+            API key as a string
+        secret:
+            API secret as a string
+        oauth_url:
+            url for OAuth2 server as a string
+        agent:
+            `User-agent` parameter to be passed in request header.
+            Default is 'bookops-overdrive/{version}'
 
     """
 
@@ -23,27 +32,28 @@ class OverdriveAccessToken:
         self,
         key: str,
         secret: str,
-        resource_url: str,
         agent: str = f"{__title__}/{__version__}",
     ) -> None:
         self.agent = agent
-        self.key = key
-        self.resource_url = resource_url
-        self.secret = secret
         self.expires_at: datetime.datetime | None = None
+        self.key = key
+        self.oauth_url = "https://oauth.overdrive.com/token"
+        self.secret = secret
         self.server_response: requests.Response | None = None
         self.token_str: str | None = None
-        self.token_type: str | None = None
 
         self._request_token()
 
     def _calculate_expiration_time(self, expires_in: int) -> datetime.datetime:
         """
         Calculates expiration time of access token based on 'expires_in'
-        timedelta from token response.
+        value from token response.
 
         Args:
             expires_in: number of seconds until the access token expires as an int
+
+        Returns:
+            expiration time of access token as `datetime.datetime` object.
         """
         expires_at = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(
             seconds=expires_in - 1
@@ -53,39 +63,40 @@ class OverdriveAccessToken:
     def _parse_server_response(self, response: requests.Response) -> None:
         """Parse response from authorization server."""
         self.server_response = response
-        if response.status_code == requests.codes.ok:
-            json_response = response.json()
-            self.token_str = json_response["access_token"]
-            self.expires_at = self._calculate_expiration_time(
-                json_response["expires_in"]
-            )
-            self.scope = json_response["scope"]
-            self.token_type = json_response["token_type"]
+        if response.ok:
+            json_resp = response.json()
+            self.token_str = json_resp["access_token"]
+            self.expires_at = self._calculate_expiration_time(json_resp["expires_in"])
 
     def _post_token_request(self) -> requests.Response:
         """Sends a POST request for an access token"""
+        auth = (self.key, self.secret)
+        headers = {"User-Agent": self.agent, "Accept": "application/json"}
+        data = {"grant_type": "client_credentials"}
         try:
             response = requests.post(
-                self.resource_url,
-                auth=(self.key, self.secret),
-                headers={"User-Agent": self.agent, "Accept": "application/json"},
-                data={"grant_type": "client_credentials"},
+                self.oauth_url, auth=auth, headers=headers, data=data
             )
+            response.raise_for_status()
+        except (requests.Timeout, requests.ConnectionError):
+            raise BookopsOverdriveError(f"Error connecting: {sys.exc_info()[0]}")
+        except requests.HTTPError as exc:
+            raise BookopsOverdriveError(
+                f"{exc}. Server response: {response.content.decode('utf-8')}"
+            )
+        else:
             return response
-
-        except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
-            raise
 
     def _request_token(self):
         """Requests access token and parses response."""
         response = self._post_token_request()
         self._parse_server_response(response)
 
+    @property
     def is_expired(self) -> bool:
-        """Checks if the access token is expired."""
-        if self.expires_at and self.expires_at < datetime.datetime.now(
-            datetime.timezone.utc
-        ):
+        """Checks if the access token has expired."""
+        now = datetime.datetime.now(datetime.timezone.utc)
+        if self.expires_at and self.expires_at < now:
             return True
         else:
             return False
---
+++ tests/test_authorization.py
@@ -0,0 +1,61 @@
+import datetime
+import os
+
+import pytest
+
+from bookops_overdrive.authorize import OverdriveAccessToken
+from bookops_overdrive.errors import BookopsOverdriveError
+
+
+class TestOverdriveAccessToken:
+    def test_successful_token_request(self, post_token_response_success):
+        token = OverdriveAccessToken(key="foo", secret="bar")
+        assert token.server_response.status_code == 200
+        assert sorted(list(token.server_response.json().keys())) == [
+            "access_token",
+            "expires_in",
+            "scope",
+            "token_type",
+        ]
+        assert "access_token: 'foo', expires_at: " in str(token)
+
+    def test_invalid_token_request(self, post_token_response_failure):
+        with pytest.raises(BookopsOverdriveError) as exc:
+            OverdriveAccessToken(key="foo", secret="bar")
+        assert "401 Client Error: Unauthorized for url: " in str(exc.value)
+
+    def test_token_request_connection_error(self, mock_connection_error):
+        with pytest.raises(BookopsOverdriveError) as exc:
+            OverdriveAccessToken(key="foo", secret="bar")
+        assert "Error connecting: " in str(exc.value)
+
+
+@pytest.mark.livetest
+@pytest.mark.usefixtures("live_creds")
+class TestOverdriveAccessTokenLive:
+    def test_authorization(self):
+        token = OverdriveAccessToken(
+            key=os.environ["CLIENT_KEY"],
+            secret=os.environ["CLIENT_SECRET"],
+        )
+        assert token.server_response.status_code == 200
+        assert token.token_str is not None
+        assert token.expires_at > datetime.datetime.now(datetime.timezone.utc)
+        assert (
+            token.server_response.json()["scope"]
+            == "LIB META AVAIL SRCH ENABLEODAPPDOWNLOADS"
+        )
+        assert sorted(list(token.server_response.json().keys())) == [
+            "access_token",
+            "expires_in",
+            "scope",
+            "token_type",
+        ]
+
+    def test_authorization_failure(self):
+        with pytest.raises(BookopsOverdriveError) as exc:
+            OverdriveAccessToken(key="key", secret="secret")
+        assert (
+            str(exc.value)
+            == '401 Client Error: Unauthorized for url: https://oauth.overdrive.com/token. Server response: {"error":"invalid_client","error_description":"The client secret was incorrect."}'
+        )
