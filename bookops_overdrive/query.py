7ce4d17745cc8d02774f3d5ac175505c179120d7
Charlotte Kostelic <charlottekostelic@gmail.com>
2025-08-08 12:45:59 -0400

    added `timeout` to `Query` args and expanded docstrings

--- bookops_overdrive/query.py
+++ bookops_overdrive/query.py
@@ -1,4 +1,4 @@
-"""Wrapper class to handle access token refresh and manage queries sent to web service."""
+"""Wrapper class to manage access token refresh and requests sent to web service."""
 
 from __future__ import annotations
 
@@ -15,21 +15,35 @@ if TYPE_CHECKING:
 
 class Query:
     """
-    The `Query` class handles access token refresh and sends requests
-    to the Overdrive API.
+    The `Query` class handles requests sent to the Overdrive API. The class checks
+    if the session's associated access token has expired before sending the request.
+    This ensures that requests will always be sent with an unexpired token.
 
     Args:
-        session: An `OverdriveSession` object
-        prepared_request: the request that will be sent to the web service
+        session:
+            An `OverdriveSession` object.
+        prepared_request:
+            The request that will be sent to the web service as a
+            `requests.PreparedRequest` object.
+        timeout:
+            How many seconds to wait for the server to respond. Accepts a single value
+            to be applied to both connect and read timeouts or two separate values.
+            Default is 5 seconds for connect and read timeouts.
+
+    Raises:
+        BookopsOverdriveError: If the request encounters any errors.
     """
 
     def __init__(
-        self, session: OverdriveSession, prepared_request: requests.PreparedRequest
+        self,
+        session: OverdriveSession,
+        prepared_request: requests.PreparedRequest,
+        timeout: int | float | tuple[int | float, int | float] | None = (5, 5),
     ) -> None:
         if session.authorization.is_expired:
             session._request_new_access_token()
         try:
-            self.response = session.send(prepared_request)
+            self.response = session.send(prepared_request, timeout=timeout)
             self.response.raise_for_status()
         except (requests.Timeout, requests.ConnectionError):
             raise BookopsOverdriveError(f"Error connecting: {sys.exc_info()[0]}")
